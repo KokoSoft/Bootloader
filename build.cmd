f1f870456664f69fc31ddcef3b52a650078e2831
Koko <koko@kokosoftware.pl>
2023-01-24 17:57:02 +0100

    Build script

--- build.cmd
+++ build.cmd
@@ -1,1 +1,4 @@
-c:\Program Files\Microchip\xc8\v2.36\pic-as\bin\pic-as.exe -mcpu=PIC18F67J60
+::"c:\Program Files\Microchip\xc8\v2.40\pic-as\bin\pic-as.exe" -mcpu=PIC18F67J60 functions.S
+cd out
+"c:\Program Files\Microchip\xc8\v2.40\pic-as\bin\pic-as.exe" -mcpu=PIC18F67J60 -Wl,-Map=bootloader.map,-ABOOT=1FC00h-1FFF7h,-AETH=0h-1FFFh,-Pmem_init=BOOT ../functions.S ../boot.S ../meminit.s -msummary=+mem,+psect,+class,-hex,-file
+
