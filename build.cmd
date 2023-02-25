225f89901d4cf954bac996ca15b2db64c9beb81d
Koko <koko@kokosoftware.pl>
2023-02-25 17:58:21 +0100

    Moved chip configuration words to separate file

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
 ::"c:\Program Files\Microchip\xc8\v2.40\pic-as\bin\pic-as.exe" -mcpu=PIC18F67J60 functions.S
 cd out
-"c:\Program Files\Microchip\xc8\v2.40\pic-as\bin\pic-as.exe" -mcpu=PIC18F67J60 -Wl,-Map=bootloader.map,-ABOOT=1FC00h-1FFF7h,-AETH=0h-1FFFh,-Pmem_init=BOOT ../functions.S ../boot.S ../meminit.s -msummary=+mem,+psect,+class,-hex,-file
+"c:\Program Files\Microchip\xc8\v2.40\pic-as\bin\pic-as.exe" -mcpu=PIC18F67J60 -Wl,-Map=bootloader.map,-ABOOT=1FC00h-1FFF7h,-AETH=0h-1FFFh,-Pmem_init=BOOT ../functions.S ../boot.S ../meminit.s ../chip_config.asm -msummary=+mem,+psect,+class,-hex,-file
 
